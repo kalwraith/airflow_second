c126cc6cd97d598944a67a31948378c2e98bf86a
hyunjinkim <kalwraith@naver.com>
2023-02-23 20:10:58 +0900

    task 연결 테스트

---
+++ dags/dags_task_conn_test.py
@@ -0,0 +1,51 @@
+# Package Import
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(dag_id='dags_task_conntest',
+          start_date=datetime(2023,2,16),
+          schedule_interval=None,
+          catchup=False
+) as dag:
+    t1 = EmptyOperator(
+        task_id='dummy_t1',
+    )
+
+    t2 = EmptyOperator(
+        task_id='dummy_t2',
+    )
+
+    t3 = EmptyOperator(
+        task_id='dummy_t3',
+    )
+
+    t4 = EmptyOperator(
+        task_id='dummy_t4',
+    )
+
+    t5 = EmptyOperator(
+        task_id='dummy_t5',
+    )
+
+    t6 = EmptyOperator(
+        task_id='dummy_t6',
+    )
+
+    t7 = EmptyOperator(
+        task_id='dummy_t7',
+    )
+
+    t8 = EmptyOperator(
+        task_id='dummy_t8',
+    )
+    
+    t1 >> t2
+    t2 >> t3
+    t2 >> t4
+    t3 >> t4
+    t5 >> t4
+    t4 >> t6
+    t4 >> t7
+    t6 >> t7
+    t8 >> t7
