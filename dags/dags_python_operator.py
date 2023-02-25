11aefe4f1129c45d46f63cad8e4562a7fa1da833
hyunjinkim <kalwraith@naver.com>
2023-02-25 21:17:54 +0900

    python operator paremter 추가

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -2,9 +2,13 @@ from airflow import DAG
 import datetime
 from airflow.operators.python import PythonOperator
 
-def sample_function():
+def sample_function(**kwargs):
     for i in range(0,10):
         print(i)
+    name_value = kwargs['name']
+    address_value = kwargs['address']
+    print('name:' + name_value)
+    print('address:' + address_value)
 
 with DAG(
     dag_id='dags_python_operator',
@@ -14,7 +18,8 @@ with DAG(
 ) as dag:
     python_task_1 = PythonOperator(
         task_id='python_task_1',
-        python_callable=sample_function
+        python_callable=sample_function,
+        op_kwargs={'name':'hjkim','address':'seoul'}
     )
 
     python_task_1
